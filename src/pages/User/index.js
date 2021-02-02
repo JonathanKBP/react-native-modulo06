6efae4a7212473fb2a4d4b9697ef6372ba124281
Jonathan Kevin <jonathan.kevinbp@hotmail.com>
2021-02-02 12:14:41 -0300

    Listando Favoritos

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   constructor() {
@@ -12,9 +24,6 @@ export default class User extends Component {
       stars: [],
     };
   }
-  // const { user } = route.params;
-
-  // navigation.setOptions({ title: user.name });
 
   async componentDidMount() {
     const { navigation, route } = this.props;
@@ -28,7 +37,32 @@ export default class User extends Component {
 
   render() {
     const { stars } = this.state;
-    return <View />;
+    const { route } = this.props;
+
+    const { user } = route.params;
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+        <Stars
+          data={stars}
+          keyExtractor={(star) => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
 
@@ -41,6 +75,8 @@ User.propTypes = {
       user: PropTypes.shape({
         name: PropTypes.string,
         login: PropTypes.string,
+        avatar: PropTypes.string,
+        bio: PropTypes.string,
       }),
     }),
   }).isRequired,
